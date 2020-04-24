9148aa4d6123eef98d2568092f6c8545982bb776
neutralix <martin123514@yahoo.com>
2020-04-24 14:58:32 +0700

    enkrip1 done

--- enkrip1.c
+++ enkrip1.c
@@ -11,6 +11,49 @@
 static const char *dirpath = "/home/martin/Documents";
 static const char *key = "9(ku@AW1[Lmvgax6q`5Y2Ry?+sF!^HKQiBXCUSe&0M.b%rI'7d)o4~VfZ*{#:}ETt$3J-zpc]lnh8,GwP_ND|jO";
 
+void dekrip1(char* res, char* name) {
+    strcpy(res, "");
+    int resmod = 87;
+    int dekrip = 1;
+    char* new = name;
+    int slash = 0;
+    if (name[0] == '/') {
+        name++;
+        slash = 1;
+    }
+    int first = 1;
+    char* tok;
+    tok = strtok(new, "/");
+    while (tok != NULL) {
+        char rev[1000];
+        int index = 0;
+        for (int i = 0; i < strlen(tok); i++) {
+            if (tok[i]=='.') {
+                dekrip = 0;
+            }
+            if (dekrip) {
+                int pos = 0, z = 0;
+                while(1) {
+                    if (key[z]==tok[i])
+                        break;
+                    z++;
+                }
+                pos = (z + 87 - 10) % resmod;
+                rev[index] = key[pos];
+            }
+            else {
+                rev[index] = tok[i];
+            }
+            index++;
+        }
+        rev[index] = '\0';
+        if (!first || slash) strcat(res, "/");
+        first = 0;
+        strcat(res, rev);
+        tok = strtok(NULL, "/");
+    }
+}
+
 void enkrip1(char* res, char* name) {
     strcpy(res, "");
     int resmod = 87;
@@ -31,7 +74,6 @@ void enkrip1(char* res, char* name) {
             if (tok[i]=='.') {
                 enkrip = 0;
             }
-
             if (enkrip) {
                 int pos = 0, z = 0;
                 while(1) {
@@ -56,11 +98,27 @@ void enkrip1(char* res, char* name) {
 }
 
 static int xmp_getattr(const char *path, struct stat *stbuf) {
-    int res;
+    int res,enable=0;
     char fpath[1000];
+    char befencv[1000] = "", afencv[1000] = "";
     char s[1000];
-    enkrip1(s, path);
-    sprintf(fpath, "%s/%s", dirpath, s);
+
+    char *tok = strtok(path, "/");
+    while (tok!=NULL) {
+        if (enable==0) {
+            strcat(befencv, "/");
+            strcat(befencv, tok);
+        }
+        else if (enable==1) {
+            strcat(afencv, "/");
+            strcat(afencv, tok);
+        }
+        if (strncmp(tok, "encv1_", 6) == 0)
+            enable=1;
+        tok = strtok(NULL, "/");
+    }
+    dekrip1(s, afencv);
+    sprintf(fpath, "%s%s%s", dirpath, befencv, s);
     res = lstat(fpath, stbuf);
     if (res == -1)
         return -errno;
@@ -69,20 +127,42 @@ static int xmp_getattr(const char *path, struct stat *stbuf) {
 
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
     char fpath[1000];
+    int enable=0;
     if(strcmp(path,"/") == 0) {
         path=dirpath;
         sprintf(fpath,"%s",path);
     }
     else {
         char s[1000];
-        enkrip1(s, path);
-        sprintf(fpath, "%s/%s", dirpath, s);
+        char befencv[1000] = "", afencv[1000] = "";
+
+        char *tok = strtok(path, "/");
+        while (tok!=NULL) {
+            if (enable==0) {
+                strcat(befencv, "/");
+                strcat(befencv, tok);
+            }
+            else if (enable==1) {
+                strcat(afencv, "/");
+                strcat(afencv, tok);
+            }
+
+            if (strncmp(tok, "encv1_", 6) == 0)
+                enable=1;
+            tok = strtok(NULL, "/");
+        }
+        
+        dekrip1(s, afencv);
+        sprintf(fpath, "%s%s%s", dirpath, befencv, s);
+
+        printf("%s %s %s %s\n",dirpath, befencv, s, fpath);
     }
     int res = 0;
     DIR *dp;
     struct dirent *de;
     (void) offset;
     (void) fi;
+    printf("%s\n", fpath);
     dp = opendir(fpath);
     if (dp == NULL) {
         return -errno;
@@ -92,9 +172,16 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
         memset(&st, 0, sizeof(st));
         st.st_ino = de->d_ino;
         st.st_mode = de->d_type << 12;
-        char s[1000];
-        enkrip1(s, de->d_name);
-        res = (filler(buf, s, &st, 0));
+
+        if (enable==1) {
+            char sa[1000];
+            enkrip1(sa, de->d_name);
+            res = (filler(buf, sa, &st, 0));
+        }
+        else {
+            res = (filler(buf, de->d_name, &st, 0));
+        }
+
         if(res!=0) break;
     }
     closedir(dp);
@@ -109,8 +196,28 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset, stru
     }
     else {
         char s[1000];
-        enkrip1(s, path);
-        sprintf(fpath, "%s/%s", dirpath, s);
+        int enable=0;
+        char fpath[1000];
+        char befencv[1000] = "", afencv[1000] = "";
+
+        char *tok = strtok(path, "/");
+        while (tok!=NULL) {
+            if (enable==0) {
+                strcat(befencv, "/");
+                strcat(befencv, tok);
+            }
+            else if (enable==1) {
+                strcat(afencv, "/");
+                strcat(afencv, tok);
+            }
+
+            if (strncmp(tok, "encv1_", 6) == 0)
+                enable=1;
+            tok = strtok(NULL, "/");
+        }
+        
+        dekrip1(s, afencv);
+        sprintf(fpath, "%s%s%s", dirpath, befencv, s);
     }
     int res = 0;
     int fd = 0 ;
